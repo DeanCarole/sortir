1ad83e149c0ce75dff10f868570184c5749010cc
agillet2022 <audreymaud.gillet2022@campus-eni.fr>
2023-03-07 10:17:44 +0100

    Entité

---
+++ src/Entity/Event.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $startDateTime = null;
+
+    #[ORM\Column]
+    private ?int $duration = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $registrationDeadline = null;
+
+    #[ORM\Column]
+    private ?int $nbRegistrationMax = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $eventData = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $state = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStartDateTime(): ?\DateTimeInterface
+    {
+        return $this->startDateTime;
+    }
+
+    public function setStartDateTime(\DateTimeInterface $startDateTime): self
+    {
+        $this->startDateTime = $startDateTime;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(int $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getRegistrationDeadline(): ?\DateTimeInterface
+    {
+        return $this->registrationDeadline;
+    }
+
+    public function setRegistrationDeadline(\DateTimeInterface $registrationDeadline): self
+    {
+        $this->registrationDeadline = $registrationDeadline;
+
+        return $this;
+    }
+
+    public function getNbRegistrationMax(): ?int
+    {
+        return $this->nbRegistrationMax;
+    }
+
+    public function setNbRegistrationMax(int $nbRegistrationMax): self
+    {
+        $this->nbRegistrationMax = $nbRegistrationMax;
+
+        return $this;
+    }
+
+    public function getEventData(): ?string
+    {
+        return $this->eventData;
+    }
+
+    public function setEventData(?string $eventData): self
+    {
+        $this->eventData = $eventData;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
