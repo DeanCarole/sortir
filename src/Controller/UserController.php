d5b93f31aecb82f535c16a7da1671c16339e71c2
agillet2022 <audreymaud.gillet2022@campus-eni.fr>
2023-03-08 15:36:01 +0100

    Update Profil avec campus

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,23 +13,52 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/user', name: 'user_')]
 class UserController extends AbstractController
 {
-    #[Route('/show/{id}', name: 'show', requirements: ['id' => '\d+'])]
+    #[Route('/show', name: 'show')]
     public function show(int $id): Response
     {
         return $this->render('user/show.html.twig');
     }
- //Pour modifier l'utilisateur
-    #[Route('/update', name: 'update')]
-    public function update(UserRepository $userRepository): Response
-    {
-        // récupérer l'utilisateur' et le renvoyer via l'id
-       // $user = $userRepository->find($id);
-        $user = $this->getUser();
+ //Pour afficher l'utilisateur modifiable
+//    #[Route('/update', name: 'update')]
+//    public function update(Request $request,UserRepository $userRepository): Response
+//    {
+//        $user = $this->getUser();
+//        $userForm = $this->createForm(UserType::class, $user);
+//
+//        return $this->render('user/update.html.twig',['user' => $user,
+//            'userForm' => $userForm->createView()
+//        ]);
+//    }
 
+    //Pour modifier l'utilisateur dans la BDD
+    #[Route('/update', name: 'update')]
+    public function update(Request $request,UserRepository $userRepository): Response
+    {
+        $user = $this->getUser();
         $userForm = $this->createForm(UserType::class, $user);
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            // récupérer les données du formulaire
+            $user = $userForm->getData();
+
+            // mettre à jour les champs modifiables de l'utilisateur
+            $user->setUsername($user->getUsername());
+            $user->setName($user->getName());
+            $user->setFirstName($user->getFirstName());
+            $user->setPhone($user->getPhone());
+            $user->setEmail($user->getEmail());
+            $user->setPassword($user->getPassword());
+            $userRepository->save($user,true);
+            $this->addFlash('success',"Profil Update !");
+
+            // rediriger l'utilisateur vers une autre page
+            return $this->redirectToRoute('main_home');
+        }
 
         return $this->render('user/update.html.twig',['user' => $user,
             'userForm' => $userForm->createView()
         ]);
     }
+
 }
