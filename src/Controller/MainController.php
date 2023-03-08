687146170bb08646699c9adde1899dc79385b5fd
dean <carole.dean2022@campus-eni.fr>
2023-03-08 11:06:24 +0100

    création de la liste des évènements

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,8 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/home', name: 'main_home')]
-    public function home(): Response
+    public function home(EventRepository $eventRepository): Response
     {
-        return $this->render('main/home.html.twig');
+    $events = $eventRepository->findAll();
+
+        return $this->render('main/home.html.twig', ['events'=>$events]);
     }
 }
