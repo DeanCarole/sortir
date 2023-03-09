cd33069f6a5716e8242979a593be1c97f8e7d1f5
dean <carole.dean2022@campus-eni.fr>
2023-03-09 14:22:13 +0100

    fin service état !!!!!

--- src/Services/Update.php
+++ src/Services/Update.php
@@ -31,21 +31,36 @@ class Update
        $event1 = clone $event->getStartDateTime();
        $event1->modify("+" .$event->getDuration() . "minute");
 
+       if($event->getId() == 1){
+           dump($event);
+           dump($event1);
+       }
+
         //condition pour modifier l'évènement
-        if ($event->getStartDateTime() < new \DateTime('-1 month') ) {
+        //cas où ça date de plus de 1 mois
+        if ($event1 < new \DateTime('-1 month') ) {
+
             $state = $this->stateRepository->findOneBy(['label'=>'archived']);
             $event->setState($state);
-        } else  if (($event->getStartDateTime() < new \DateTime()) && ($event->getStartDateTime() > new \DateTime('-1 month')))  {
+            //entre 1 mois et aujourd'hui
+        } else  if ($event1 < new \DateTime())  {
+
             $state = $this->stateRepository->findOneBy(['label'=>'finished']);
             $event->setState($state);
         //} else if ((new \DateTime('now') >= $event->getStartDateTime()) &&  ( $event->getStartDateTime() < $event1 )) {
-        } else if (($event->getStartDateTime() >= new \DateTime()) && ($event->getStartDateTime()< $event1 )) {
+        } else if ($event->getStartDateTime() < new \DateTime()) {
+            dump($event);
             $state = $this->stateRepository->findOneBy(['label'=>'inProgress']);
             $event->setState($state);
         } else if ((new \DateTime() > $event->getRegistrationDeadline())){
+
             $state = $this->stateRepository->findOneBy(['label'=>'closed']);
             $event->setState($state);
         }
+
+//        if($event->getId() == 9){
+//            dump($event);
+//        }
 
         //permet de faire le persist et le flush -> modifie la base de données
         $this->eventRepository->save($event, true);
