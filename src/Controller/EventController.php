271e20853a31f1b9b81fd9e3a23b817529bc63c6
dean <carole.dean2022@campus-eni.fr>
2023-03-09 16:53:46 +0100

    début formulaire créer une sortie

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Form\EventType;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,9 +14,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class EventController extends AbstractController
 {
     #[Route('/add', name: 'add')]
-    public function index(): Response
+    public function add( Request $request, EventRepository $eventRepository): Response
     {
-        return $this->render('event/add.html.twig');
+        $event = new Event();
+        $eventForm = $this->createForm(EventType::class, $event);
+        $eventForm->handleRequest($request);
+
+        if ($eventForm->isSubmitted() && $eventForm->isValid()) {
+            // récupérer les données du formulaire
+            $event = $eventForm->getData();
+
+            // Modifier le mot de passe si le champ est rempli
+            $password = $eventForm->get('password')->getData();
+
+
+            $eventRepository->save($event, true);
+            $this->addFlash('success', "Profil modifié !");
+
+
+        }
+
+        return $this->render('event/add.html.twig', [
+            'eventForm' => $eventForm->createView()
+        ]);
     }
 
     #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
