77e6cf5a0cf21855c02108101aeafde7375c5647
falbricias <f.albricias@laposte.net>
2023-03-09 11:26:00 +0100

    MAJ françois : consultation sortie

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,4 +15,25 @@ class EventController extends AbstractController
     {
         return $this->render('event/add.html.twig');
     }
+
+    #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
+    public function show(int $id, EventRepository $eventRepository): Response
+    {
+        //Récupération d'un event par son id
+        $event = $eventRepository->find($id);
+
+        if(!$event){
+            //Lance une erreur 404 si l'event n'existe pas
+            throw $this->createNotFoundException("Oups ! Cette sortie n'existe pas !");
+        }
+
+        return $this->render('event/show.html.twig', [
+            'event' => $event
+        ]);
+    }
+
+
+
+
+
 }
