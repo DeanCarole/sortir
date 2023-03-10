25f7838804a3bf7ded78bb6640aeaa90a32e479e
dean <carole.dean2022@campus-eni.fr>
2023-03-10 10:15:28 +0100

    suite formulaire créer une sortie ajout css bouton créer une sortie

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -10,6 +10,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -21,35 +22,31 @@ class EventType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
-           ->add('startDateTime',  DateTimeType::class)
-            ->add('registrationDeadline', DateType::class)
-            ->add('nbRegistrationMax', TextType::class)
-            ->add('duration', TextType::class)
-            ->add('eventData', TextareaType::class)
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la sortie : '
+            ])
+           ->add('startDateTime',  DateTimeType::class, [
+               'label' => 'Date et heure de la sortie : '
+           ])
+            ->add('registrationDeadline', DateType::class, [
+                'label' => "Date limite d'inscription : "
+            ])
+            ->add('nbRegistrationMax', TextType::class, [
+                'label' => 'Nombre de places : '
+            ])
+            ->add('duration', TextType::class, [
+                'label' => 'Durée : '
+            ])
+            ->add('eventData', TextareaType::class, [
+                'label' => 'Description et infos : '
+            ])
             ->add('campus', EntityType::class, ['class' => Campus::class,
                 'choice_label' => 'name',
-                'label' => 'Campus'
+                'label' => 'Campus : '
             ])
-            ->add ('place', EntityType::class, [
-                'class' => Place::class,
-                'choice_label'=>'name',
-                'label' => 'Choisir un lieu'
-            ])
-//            ->add('place', EntityType::class, [
-//            "class"=> Place::class,
-//                'choice_label'=>'name',
-//            "label"=> "Choisir un lieu"
-//            ])
-            ->add('place', EntityType::class, [
-                "class"=> Place::class,
-                'choice_label'=>'latitude',
-                "label"=> "Choisir une latitude"
-            ])
-            ->add('place', EntityType::class, [
-                "class"=> Place::class,
-                'choice_label'=>'longitude',
-                "label"=> "Choisir une longitude"
+            ->add ('place', EntityType::class, ['class' => Place::class,
+                'choice_label' => 'name',
+                'label' => 'Lieu : '
             ])
         ;
     }
