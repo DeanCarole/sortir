382e6b759bfb291340b9da12c868dd0f90fa1edb
dean <carole.dean2022@campus-eni.fr>
2023-03-13 16:32:02 +0100

    modification update

--- src/Services/Update.php
+++ src/Services/Update.php
@@ -21,18 +21,17 @@ class Update
     }
 
 
-    public function updateState() {
+    public function updateState(): void
+    {
     //modifier statut d'un évènement suivant la date de début de la sortie
 
     //récupère toutes les sorties avec le repository
     $events = $this->eventRepository->findAllEvents();
 
-    $states = $this->stateRepository->findAll();
-
     //boucle sur chaque évènement
     foreach ($events as $event) {
 
-        foreach ($states as $state){
+
 
         //récupère le statut de l'évènement
         $status = $event->getState()->getLabel();
@@ -40,37 +39,40 @@ class Update
        $event1 = clone $event->getStartDateTime();
        $event1->modify("+" .$event->getDuration() . "minute");
 
-//       if($event->getId() == 1){
-//           dump($event);
-//           dump($event1);
-//       }
+       if($event->getId() == 1){
+           dump($event);
+       }
 
         //condition pour modifier l'évènement
         //cas où ça date de plus de 1 mois
-        if ($event1 < new \DateTime('-1 month') ) {
-            $event->setState($state->setLabel('archived'));
+        if ($event1 < new DateTime('-1 month') ) {
+            $state = $this->stateRepository->findOneBy(['label' => 'archived']);
+            $event->setState($state);
             //entre 1 mois et aujourd'hui
-        } else  if ($event1 < new \DateTime())  {
-            $event->setState($state->setLabel('finished'));
+        } else  if ($event1 < new DateTime())  {
+            $state = $this->stateRepository->findOneBy(['label' => 'finished']);
+            $event->setState($state);
         //} else if ((new \DateTime('now') >= $event->getStartDateTime()) &&  ( $event->getStartDateTime() < $event1 )) {
-        } else if ($event->getStartDateTime() < new \DateTime()) {
-            $event->setState($state->setLabel('inProgress'));
-        } else if ((new \DateTime() > $event->getRegistrationDeadline())){
-            $event->setState($state->setLabel('closed'));
+        } else if ($event->getStartDateTime() < new DateTime()) {
+            $state = $this->stateRepository->findOneBy(['label' => 'inProgress']);
+            $event->setState($state);
+        } else if ((new DateTime() > $event->getRegistrationDeadline())){
+            $state = $this->stateRepository->findOneBy(['label' => 'closed']);
+            $event->setState($state);
         }
 
-//        if($event->getId() == 9){
-//            dump($event);
-//        }
-
+        if($event->getId() == 1){
+            dump($event);
+        }
+           // $this->eventRepository->save($event, true);
         //permet de faire le persist et le flush -> modifie la base de données
-       $this->entityManager->persist($event);
 
+        //$this->eventRepository->save($event, true);
+        $this->entityManager->persist($event);
         }
-    }
-        $this->entityManager->flush($events);
-
-
+$this->entityManager->flush($events);
 
 }
+
+
 }
